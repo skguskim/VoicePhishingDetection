3928fe86c5cff70f2174a3cd64d942af533f5ebc
skguskim <skgus3874@seoultech.ac.kr>
2025-11-13 05:15:51 +0900

    Adds role insertion/deletion functionalities
    Introduces modules for adding and deleting role labels in conversations.
    
    These modules facilitate the process of re-inserting removed speaker identifiers and deleting victim conversation data, which can be helpful for data processing.

---
+++ data/role_deletion.py
@@ -0,0 +1,16 @@
+# data/role_deletion.py
+
+'''
+피해자 대화 내용을 삭제하는 과정
+함수 이름은 스네이크 표기법으로, 구현을 편하게 하되 설명(각주)이 적혀있어야함 
+아래와 같은 예시처럼 작성될 것
+'''
+
+def deletion(conversation):
+    pass # 여기에 로직 구현
+    
+def main():
+    pass # 여기에 로직 구현
+
+if __name__ == "__main__":
+    main()
---
+++ data/role_insertion.py
@@ -0,0 +1,16 @@
+#data/role_insertion.py
+
+'''
+이미 제거된 화자 식별자(피해자 :, 사기범 :)를 다시 삽입하는 과정
+함수 이름은 스네이크 표기법으로, 구현을 편하게 하되 설명(각주)이 적혀있어야함 
+아래와 같은 예시처럼 작성될 것
+    '''
+
+def insert_role_labels(conversation):
+    pass # 여기에 로직 구현
+    
+def main():
+    pass # 여기에 로직 구현
+
+if __name__ == "__main__":
+    main()
